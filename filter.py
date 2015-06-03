b976d2e376507e9239382b50c1679e1ef7171f07
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-06-02 19:35:49 -0700

    added tests for written functions

--- filter.py
+++ filter.py
@@ -23,3 +23,42 @@ class Filter():
         for char in word:
             total += ord(char)
         return total
+
+class TestFilterMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.arthur = Filter()
+
+    def test_hash_sum(self):
+        self.assertEqual(self.arthur.hash_sum('hello'), 532)
+        self.assertEqual(self.arthur.hash_sum('world'), 552)
+        self.assertEqual(self.arthur.hash_sum('Sivan'), 513)
+        self.assertEqual(self.arthur.hash_sum('Mehta'), 495)
+        self.assertEqual(self.arthur.hash_sum(''), 0)
+
+    def test_hash_length(self):
+        self.assertEqual(self.arthur.hash_length('Hello!'), 6)
+        self.assertEqual(self.arthur.hash_length('My'), 2)
+        self.assertEqual(self.arthur.hash_length('name'), 4)
+        self.assertEqual(self.arthur.hash_length('is'), 2)
+        self.assertEqual(self.arthur.hash_length('none of your business'), 21)
+
+    def test_hash_word(self):
+        self.assertEqual(self.arthur.hash_word("Z"),     0b00010110100000000001)
+        self.assertEqual(self.arthur.hash_word("\x01"),  0b00000000010000000001)
+        self.assertEqual(self.arthur.hash_word("Z\x01"), 0b00010110110000000010)
+        self.assertEqual(self.arthur.hash_word("frank"), 0b10000100100000000101)
+
+    def test_add(self):
+        self.arthur.add("Z")
+        self.assertEqual(self.arthur.bloom_filter,     0b00010110100000000001)
+        self.arthur.add("\x01")
+        self.assertEqual(self.arthur.bloom_filter,     0b00010110110000000001)
+        self.arthur.add("Z\x01")
+        self.assertEqual(self.arthur.bloom_filter,     0b00010110110000000011)
+        self.arthur.add("frank")
+        self.assertEqual(self.arthur.bloom_filter,     0b10010110110000000111)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFilterMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)
